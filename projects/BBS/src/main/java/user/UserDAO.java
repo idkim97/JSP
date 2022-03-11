4171ce529524b5e728cd884c43a4a9d335c74681
idkim97 <idkim9797@naver.com>
2022-03-12 03:46:20 +0900

    데이터베이스 연동
    데이터베이스 연동파일

---
+++ projects/BBS/src/main/java/user/UserDAO.java
@@ -0,0 +1,47 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public UserDAO() {
+		try {
+			// 내 컴퓨터의 로컬호스트에 있는 BBS폴더에 접근하도록 한다
+			String dbURL = "jdbc:mysql://localhost:3306/BBS";
+			String dbID = "root";
+			String dbPassword = "root";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 물음표에 유저아이디를 넣어서 실제 DB에 존재하는지, 패스워드는 맞는지 확인하는 코드
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword)) {
+					return 1; // 로그인 성공
+				}
+				else 
+					return 0; // 비밀번호 불일치
+			}
+			return -1; // 아이디가 없음
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -2; // 데이터베이스 오류
+	}
+}
